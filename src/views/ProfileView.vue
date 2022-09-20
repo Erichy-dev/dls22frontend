db7e53fc031bb5fb5b64e13007426e577407dca2
Erichy-dev <erichnyaga1@gmail.com>
2022-09-20 19:52:16 +0300

    protorype complete

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,33 +1,65 @@
 <script lang="ts" setup>
+import { UserStore } from "@/stores/UserAccount";
+import axios from "axios";
+import { ref, type Ref } from "vue";
+import { useRouter } from "vue-router";
+
+const navigate = useRouter();
+const form: Ref<HTMLFormElement | null> = ref(null);
 function updateProfile() {
-  //
+  const fmData = new FormData(form.value as HTMLFormElement);
+  const teamName = UserStore().teamName;
+  fmData.append("teamName", teamName);
+  axios({
+    method: "post",
+    url: "updateProfile/",
+    data: fmData,
+  }).then(() => {
+    navigate.push("/games");
+  });
 }
 </script>
 
 <template>
   <main class="flex flex-col">
-    <div class="flex-1 self-center flex flex-row w-4/12">
+    <div class="flex-1 self-center flex flex-row lg:w-4/12">
       <form
-        action=""
+        ref="form"
         @submit.prevent="updateProfile"
         class="bg-black bg-opacity-70 self-center flex flex-col p-4 space-y-2 w-full"
       >
         <h2 class="self-center text-3xl">UPDATE ACCOUNT</h2>
         <div class="flex flex-row justify-between">
-          <label for="profile" class="mr-2">PROFILE</label>
-          <input type="file" id="profile" />
-        </div>
-        <div class="flex flex-row justify-between">
           <label for="firstName" class="flex-1">FIRST NAME</label>
-          <input type="text" id="firstName" />
+          <input
+            class="text-black p-1"
+            type="text"
+            id="firstName"
+            name="firstName"
+          />
         </div>
         <div class="flex flex-row justify-between">
-          <label for="username" class="flex-1">SECOND NAME</label>
-          <input type="text" id="secondName" />
+          <label for="lastName" class="flex-1">LAST NAME</label>
+          <input
+            class="text-black p-1"
+            type="text"
+            id="lastName"
+            name="lastName"
+          />
         </div>
         <div class="flex flex-row justify-between">
           <label for="city" class="flex-1">CITY</label>
-          <input type="text" id="city" />
+          <input class="text-black p-1" type="text" id="city" name="city" />
+        </div>
+        <div class="flex flex-row justify-between">
+          <label for="password" class="flex-1">PASSWORD</label>
+          <input
+            class="text-black p-1"
+            type="password"
+            id="password"
+            name="password"
+            required
+          />
         </div>
         <button
           class="border-slate-500 bg-cyan-600 p-2 rounded-xl text-xl w-fit self-center"
--- src/views/TermsAndConditions.vue
+++ src/views/TermsAndConditions.vue
@@ -1,17 +1,45 @@
 <template>
   <main class="flex flex-col">
-    <div class="flex-1 border-blue-700 self-center flex flex-row">
-      <section class="self-center bg-gray-400 bg-opacity-70 p-6">
-        <h1>TERMS AND CONDITIONS</h1>
+    <section
+      class="self-center bg-gray-400 bg-opacity-70 p-6 text-black space-y-2"
+    >
+      <h1 class="font-bold">TERMS AND CONDITIONS</h1>
+      <section>
+        <h2 class="font-bold">PROCEDURE</h2>
         <ol class="list-decimal text-lg">
           <li>
-            Participants shall provide unedited screenshots as evidence to their
+            Log into DLS22 and navigate to
+            <span class="font-bold"> Friendly Matches</span>
+          </li>
+          <li>Generate a code and send it to your team mate</li>
+          <li>
+            After the match, both teams should screenshot their results and send
+            them as evidence.
+          </li>
+        </ol>
+      </section>
+      <section>
+        <h2 class="font-bold">RULES</h2>
+        <ol class="list-decimal text-lg">
+          <li>
+            Teams cannot change their
+            <span class="font-bold"
+              >team name, email, phone number and password</span
+            >
+            directly from the website.In case of such a need please
+            <span
+              class="hover:text-red-600 font-bold"
+              href="mailto:erichnyaga1@gmail.com"
+              >email us</span
+            >.
+          </li>
+          <li>
+            Teams shall provide unedited screenshots as evidence to their
             game results
           </li>
           <li>Participants must follow the rules to the latter</li>
-          <li>Violations of the terms and conditions shall not be ignored</li>
         </ol>
       </section>
-    </div>
+    </section>
   </main>
 </template>
